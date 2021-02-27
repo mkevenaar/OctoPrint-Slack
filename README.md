6e9dc97395c7bc030159e1c6d084fde078582c2f
Maurice Kevenaar <maurice@kevenaar.name>
2021-02-27 10:45:04 +0100

    (maint) repo setup

--- README.md
+++ README.md
@@ -1,25 +1,21 @@
-OctoPrint Slack Integration
-===========================
+# OctoPrint Slack Integration
 
 Send messages to your group's Slack chat when printing events happen!
 You need to set up an [Incoming Webhook](https://my.slack.com/services/new/incoming-webhook) integration on the Slack side to use this.
 
-Features
---------
+## Features
 
 * Select which events you want to trigger a chat notification for
 * Customizable messages for each event
 * Customize bot icon and username in Slack chat
 * Sends elapsed time of print after finished
 
-Screenshots
------------
+## Screenshots
 
 ![Slack chat with messages from plugin.](/screenshots/slack.png?raw=true)
 ![Plugin settings screenshot.](/screenshots/settings.png?raw=true)
 ![Plugin settings screenshot.](/screenshots/settings2.png?raw=true)
 
-Installation
-------------
+## Installation
 
 Follow the instructions provided by [OctoPrint](http://plugins.octoprint.org/help/installation/).
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 plugin_identifier = "slack"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_%s" % plugin_identifier
+plugin_package = "octoprint_slack
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
@@ -21,13 +21,13 @@ plugin_version = "0.2.3"
 plugin_description = "Slack integration for OctoPrint"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Richard Joyce"
+plugin_author = "Maurice Kevenaar"
 
 # The plugin's author's mail address.
-plugin_author_email = "rjoyce@rjoyce.me"
+plugin_author_email = "maurice@kevenaar.name"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/richjoyce/OctoPrint-Slack"
+plugin_url = "https://github.com/mkevenaar/OctoPrint-Slack"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "MIT"
@@ -35,9 +35,28 @@ plugin_license = "MIT"
 # Any additional requirements besides OctoPrint should be listed here
 plugin_requires = ['requests']
 
+### --------------------------------------------------------------------------------------------------------------------
+### More advanced options that you usually shouldn't have to touch follow after this point
+### --------------------------------------------------------------------------------------------------------------------
+
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
 # already be installed automatically if they exist.
 plugin_additional_data = []
+# Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
+plugin_addtional_packages = []
+
+# Any python packages within <plugin_package>.* you do NOT want to install with your plugin
+plugin_ignored_packages = []
+
+# Additional parameters for the call to setuptools.setup. If your plugin wants to register additional entry points,
+# define dependency links or other things like that, this is the place to go. Will be merged recursively with the
+# default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
+# octoprint.util.dict_merge.
+#
+# Example:
+#     plugin_requires = ["someDependency==dev"]
+#     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
+additional_setup_parameters = {}
 
 ########################################################################################################################
 
@@ -51,8 +70,9 @@ except:
 	import sys
 	sys.exit(-1)
 
-setup(**octoprint_setuptools.create_plugin_setup_parameters(
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
 	identifier=plugin_identifier,
+	package=plugin_package,
 	name=plugin_name,
 	version=plugin_version,
 	description=plugin_description,
@@ -61,5 +81,13 @@ setup(**octoprint_setuptools.create_plugin_setup_parameters(
 	url=plugin_url,
 	license=plugin_license,
 	requires=plugin_requires,
+	additional_packages=plugin_addtional_packages,
+	ignored_packages=plugin_ignored_packages,
 	additional_data=plugin_additional_data
-))
+)
+
+if len(additional_setup_parameters):
+	from octoprint.util import dict_merge
+	setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
+
+setup(**setup_parameters)
