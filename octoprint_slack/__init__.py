0751c5dcae3059ffcdcc855f7dded3d56cb77cbe
Maurice Kevenaar <maurice@kevenaar.name>
2021-02-27 16:26:20 +0100

    (GH-4) Python 3 compatibility

--- octoprint_slack/__init__.py
+++ octoprint_slack/__init__.py
@@ -10,181 +10,237 @@ from flask_login import current_user
 
 
 class SlackPlugin(
-	octoprint.plugin.SettingsPlugin,
-	octoprint.plugin.TemplatePlugin,
-	octoprint.plugin.EventHandlerPlugin
+        octoprint.plugin.SettingsPlugin,
+        octoprint.plugin.TemplatePlugin,
+        octoprint.plugin.EventHandlerPlugin
 ):
-	# SettingsPlugin
-	def get_settings_defaults(self):
-		return dict(
-			webhook_url="",
-			print_events=dict(
-				PrintStarted=dict(
-					Enabled=True,
-					Message="A new print has started! :muscle:",
-					Fallback="Print started! Filename: {filename}",
-					Color="good",
-				),
-				PrintFailed=dict(
-					Enabled=True,
-					Message="Oh no! The print has failed... :rage2:",
-					Fallback="Print failed! Filename: {filename}",
-					Color="danger",
-				),
-				PrintCancelled=dict(
-					Enabled=True,
-					Message="Uh oh... someone cancelled the print! :crying_cat_face:",
-					Fallback="Print cancelled! Filename: {filename}",
-					Color="danger",
-				),
-				PrintDone=dict(
-					Enabled=True,
-					Message="Print finished successfully! :thumbsup:",
-					Fallback="Print finished! Filename: {filename}, Time: {time}",
-					Color="good",
-				),
-				PrintPaused=dict(
-					Enabled=True,
-					Message="Printing has been paused... :sleeping:",
-					Fallback="Print paused... Filename: {filename}",
-					Color="warning",
-				),
-				PrintResumed=dict(
-					Enabled=True,
-					Message="Phew! Printing has been resumed! Back to work... :hammer:",
-					Fallback="Print resumed! Filename: {filename}",
-					Color="good",
-				),
-			),
-		)
+    ##~~ SettingsPlugin
+    def get_settings_defaults(self):
+        return dict(
+            webhook_url="",
+            print_events=dict(
+                PrintStarted=dict(
+                        Enabled=True,
+                        Message="A new print has started! :muscle:",
+                        Fallback="Print started! Filename: {filename}",
+                        Color="good",
+                ),
+                PrintFailed=dict(
+                    Enabled=True,
+                    Message="Oh no! The print has failed... :rage2:",
+                    Fallback="Print failed! Filename: {filename}",
+                    Color="danger",
+                ),
+                PrintCancelled=dict(
+                    Enabled=True,
+                    Message="Uh oh... someone cancelled the print! :crying_cat_face:",
+                    Fallback="Print cancelled! Filename: {filename}",
+                    Color="danger",
+                ),
+                PrintDone=dict(
+                    Enabled=True,
+                    Message="Print finished successfully! :thumbsup:",
+                    Fallback="Print finished! Filename: {filename}, Time: {time}",
+                    Color="good",
+                ),
+                PrintPaused=dict(
+                    Enabled=True,
+                    Message="Printing has been paused... :sleeping:",
+                    Fallback="Print paused... Filename: {filename}",
+                    Color="warning",
+                ),
+                PrintResumed=dict(
+                    Enabled=True,
+                    Message="Phew! Printing has been resumed! Back to work... :hammer:",
+                    Fallback="Print resumed! Filename: {filename}",
+                    Color="good",
+                ),
+            ),
+        )
 
-	def on_settings_load(self):
-		data = octoprint.plugin.SettingsPlugin.on_settings_load(self)
+    def on_settings_load(self):
+        data = octoprint.plugin.SettingsPlugin.on_settings_load(self)
 
-		# only return our restricted settings to admin users - this is only needed for OctoPrint <= 1.2.16
-		restricted = ("webhook_url",)
-		for r in restricted:
-			if r in data and (current_user is None or current_user.is_anonymous() or not current_user.is_admin()):
-				data[r] = None
+        # only return our restricted settings to admin users - this is only needed for OctoPrint <= 1.2.16
+        restricted = ("webhook_url",)
+        for r in restricted:
+            if r in data and (current_user is None or current_user.is_anonymous() or not current_user.is_admin()):
+                data[r] = None
 
-		return data
+        return data
 
-	def get_settings_restricted_paths(self):
-		# only used in OctoPrint versions > 1.2.16
-		return dict(admin=[["webhook_url"], ])
+    def get_settings_restricted_paths(self):
+        # only used in OctoPrint versions > 1.2.16
+        return dict(admin=[["webhook_url"], ])
 
-	def get_settings_version(self):
-		return 3
+    def get_settings_version(self):
+        return 3
 
-	def on_settings_migrate(self, target, current):
-		if current == 1 or current == 2:
-			events = self._settings.get(['events'])
-			# migrate events
-			print_events = self._settings.get(['print_events'])
-			if events:
-				for event in events:
-					if not events[event]:
-						self._settings.set_boolean(['print_events', event, 'Enabled'], False)
-			# remove old settings if there
-			self._settings.set(['enabled'], None)
-			self._settings.set(['events'], None)
-			# clean up old fallback messages from <1.2.7 oversaving
-			for event in print_events:
-				try:
-					self._settings.remove(['print_events', event, 'Fallback'])
-				except ValueError:
-					# Remove fallback for bug in 1.2.8 and earlier
-					self._settings.settings.remove(self._settings._prefix_path(['print_events', event, 'Fallback']))
+    def on_settings_migrate(self, target, current):
+        if current == 1 or current == 2:
+            events = self._settings.get(['events'])
+            # migrate events
+            print_events = self._settings.get(['print_events'])
+            if events:
+                for event in events:
+                    if not events[event]:
+                        self._settings.set_boolean(
+                            ['print_events', event, 'Enabled'], False)
+            # remove old settings if there
+            self._settings.set(['enabled'], None)
+            self._settings.set(['events'], None)
+            # clean up old fallback messages from <1.2.7 oversaving
+            for event in print_events:
+                try:
+                    self._settings.remove(['print_events', event, 'Fallback'])
+                except ValueError:
+                    # Remove fallback for bug in 1.2.8 and earlier
+                    self._settings.settings.remove(
+                        self._settings._prefix_path(['print_events', event, 'Fallback']))
 
-	# TemplatePlugin
-	def get_template_configs(self):
-		return [dict(type="settings", name="Slack", custom_bindings=False)]
+    ##~~ TemplatePlugin
+    def get_template_configs(self):
+        return [dict(type="settings", name="Slack", custom_bindings=False)]
 
-	# EventPlugin
-	def on_event(self, event, payload):
-		events = self._settings.get(['print_events'], merged=True)
+    ##~~ EventPlugin
+    def on_event(self, event, payload):
+        events = self._settings.get(['print_events'], merged=True)
 
-		if event in events and events[event] and events[event]['Enabled']:
+        if event in events and events[event] and events[event]['Enabled']:
 
-			webhook_url = self._settings.get(['webhook_url'])
-			if not webhook_url:
-				self._logger.exception("Slack Webhook URL not set!")
-				return
+            webhook_url = self._settings.get(['webhook_url'])
+            if not webhook_url:
+                self._logger.exception("Slack Webhook URL not set!")
+                return
 
-			filename = payload["name"]
-			if payload['origin'] == 'local':
-				origin = "Local"
-			elif payload['origin'] == 'sdcard':
-				origin = "SD Card"
-			else:
-				origin = payload['origin']
+            filename = payload["name"]
+            if payload['origin'] == 'local':
+                origin = "Local"
+            elif payload['origin'] == 'sdcard':
+                origin = "SD Card"
+            else:
+                origin = payload['origin']
 
-			message = {}
+            message = {}
 
-			# bot display settings
+            # bot display settings
 
-			# if no username is set, it will default to the webhook username
-			username = self._settings.get(['bot_username'])
-			if username:
-				message['username'] = username
+            # if no username is set, it will default to the webhook username
+            username = self._settings.get(['bot_username'])
+            if username:
+                message['username'] = username
 
-			# if an icon is set, use that. if not, use the emoji.
-			# if neither are set, it will default to the webhook icon/emoji
-			icon_url = self._settings.get(['bot_icon_url'])
-			icon_emoji = self._settings.get(['bot_icon_emoji'])
-			if icon_url:
-				message['icon_url'] = icon_url
-			elif icon_emoji:
-				message['icon_emoji'] = icon_emoji
+            # if an icon is set, use that. if not, use the emoji.
+            # if neither are set, it will default to the webhook icon/emoji
+            icon_url = self._settings.get(['bot_icon_url'])
+            icon_emoji = self._settings.get(['bot_icon_emoji'])
+            if icon_url:
+                message['icon_url'] = icon_url
+            elif icon_emoji:
+                message['icon_emoji'] = icon_emoji
 
-			# if a channel is set, use that. if not, just don't send any
-			bot_channel = self._settings.get(['bot_channel'])
-			if bot_channel:
-				if bot_channel[0] != '#':
-					bot_channel = '#' + bot_channel
-				message['channel'] = bot_channel
+            # if a channel is set, use that. if not, just don't send any
+            bot_channel = self._settings.get(['bot_channel'])
+            if bot_channel:
+                if bot_channel[0] != '#':
+                    bot_channel = '#' + bot_channel
+                message['channel'] = bot_channel
 
-			# message settings
-			message['attachments'] = [{}]
-			attachment = message['attachments'][0]
-			attachment['fields'] = []
-			attachment['fields'].append({"title": "Filename", "value": filename, "short": True})
-			attachment['fields'].append({"title": "Origin", "value": origin, "short": True})
+            # message settings
+            message['attachments'] = [{}]
+            attachment = message['attachments'][0]
+            attachment['fields'] = []
+            attachment['fields'].append(
+                {"title": "Filename", "value": filename, "short": True})
+            attachment['fields'].append(
+                {"title": "Origin", "value": origin, "short": True})
 
-			# event settings
-			event = self._settings.get(['print_events', event], merged=True)
+            # event settings
+            event = self._settings.get(['print_events', event], merged=True)
 
-			import datetime
-			import octoprint.util
-			if "time" in payload and payload["time"]:
-				elapsed_time = octoprint.util.get_formatted_timedelta(datetime.timedelta(seconds=payload["time"]))
-			else:
-				elapsed_time = ""
+            import datetime
+            import octoprint.util
+            if "time" in payload and payload["time"]:
+                elapsed_time = octoprint.util.get_formatted_timedelta(
+                    datetime.timedelta(seconds=payload["time"]))
+            else:
+                elapsed_time = ""
 
-			attachment['fallback'] = event['Fallback'].format(**{'filename': filename, 'time': elapsed_time})
-			attachment['pretext'] = event['Message']
-			attachment['color'] = event['Color']
-			if elapsed_time != "":
-				attachment['fields'].append({"title": "Time", "value": elapsed_time, "short": True})
+            attachment['fallback'] = event['Fallback'].format(
+                **{'filename': filename, 'time': elapsed_time})
+            attachment['pretext'] = event['Message']
+            attachment['color'] = event['Color']
+            if elapsed_time != "":
+                attachment['fields'].append(
+                    {"title": "Time", "value": elapsed_time, "short": True})
 
-			self._logger.debug("Attempting post of Slack message: {}".format(message))
-			try:
-				res = requests.post(webhook_url, data=json.dumps(message))
-			except Exception as e:
-				self._logger.exception("An error occurred connecting to Slack:\n {}".format(e))
-				return
+            self._logger.debug(
+                "Attempting post of Slack message: {}".format(message))
+            try:
+                res = requests.post(webhook_url, data=json.dumps(message))
+            except Exception as e:
+                self._logger.exception(
+                    "An error occurred connecting to Slack:\n {}".format(e))
+                return
 
-			if not res.ok:
-				self._logger.exception("An error occurred posting to Slack:\n {}".format(res.text))
-				return
+            if not res.ok:
+                self._logger.exception(
+                    "An error occurred posting to Slack:\n {}".format(res.text))
+                return
 
-			self._logger.debug("Posted event successfully to Slack!")
+            self._logger.debug("Posted event successfully to Slack!")
 
-		else:
-			self._logger.debug("Slack not configured for event.")
-			return
+        else:
+            self._logger.debug("Slack not configured for event.")
+            return
+
+    # ~~ Softwareupdate hook
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+        # for details.
+        return dict(
+            Slack=dict(
+                displayName="Slack",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="mkevenaar",
+                repo="OctoPrint-Slack",
+                current=self._plugin_version,
+
+                # stable releases
+                stable_branch=dict(
+                    name="Stable",
+                    branch="main",
+                    comittish=["main"]
+                ),
+
+                # release candidates
+                prerelease_branches=[
+                    dict(
+                        name="Development",
+                        branch="devel",
+                        comittish=["devel", "main"],
+                    )
+                ],
+                # update method: pip
+                pip="https://github.com/mkevenaar/OctoPrint-Slack/archive/{target_version}.zip"
+            )
+        )
 
 
 __plugin_name__ = "Slack"
 __plugin_pythoncompat__ = ">=2.7,<4"
-__plugin_implementation__ = SlackPlugin()
+
+
+def __plugin_load__():
+    global __plugin_implementation__
+    global __plugin_hooks__
+
+    __plugin_implementation__ = SlackPlugin()
+
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+    }
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 ########################################################################################################################
-### Do not forget to adjust the following variables to your own plugin.
+# Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
 plugin_identifier = "slack"
@@ -35,16 +35,18 @@ plugin_license = "MIT"
 # Any additional requirements besides OctoPrint should be listed here
 plugin_requires = ['requests']
 
-### --------------------------------------------------------------------------------------------------------------------
-### More advanced options that you usually shouldn't have to touch follow after this point
-### --------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
+# More advanced options that you usually shouldn't have to touch follow after this point
+# --------------------------------------------------------------------------------------------------------------------
 
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
-# already be installed automatically if they exist.
+# already be installed automatically if they exist. Note that if you add something here you'll also need to update
+# MANIFEST.in to match to ensure that python setup.py sdist produces a source distribution that contains all your
+# files. This is sadly due to how python's setup.py works, see also http://stackoverflow.com/a/14159430/2028598
 plugin_additional_data = []
 
-# Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
-plugin_addtional_packages = []
+# Any additional python packages you need to install with your plugin that are not contained in <plugin_package>.*
+plugin_additional_packages = []
 
 # Any python packages within <plugin_package>.* you do NOT want to install with your plugin
 plugin_ignored_packages = []
@@ -59,41 +61,37 @@ plugin_ignored_packages = []
 #     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
 additional_setup_parameters = {}
 
-# README/long description file to use for PyPi uploads. Must be the full absolute path. If the filename ends on
-# .md and pypandoc is installed a conversion from Markdown to ReStructured Text will be performed utilizing
-# setuptools-markdown as additional setup requirement.
-plugin_readme_file = "README.md"
-
 ########################################################################################################################
 
 from setuptools import setup
 
 try:
-	import octoprint_setuptools
+    import octoprint_setuptools
 except:
-	print("Could not import OctoPrint's setuptools, are you sure you are running that under "
-	      "the same python installation that OctoPrint is installed under?")
-	import sys
-	sys.exit(-1)
+    print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+          "the same python installation that OctoPrint is installed under?")
+    import sys
+    sys.exit(-1)
 
 setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
-	identifier=plugin_identifier,
-	package=plugin_package,
-	name=plugin_name,
-	version=plugin_version,
-	description=plugin_description,
-	author=plugin_author,
-	mail=plugin_author_email,
-	url=plugin_url,
-	license=plugin_license,
-	requires=plugin_requires,
-	additional_packages=plugin_addtional_packages,
-	ignored_packages=plugin_ignored_packages,
-	additional_data=plugin_additional_data
+    identifier=plugin_identifier,
+    package=plugin_package,
+    name=plugin_name,
+    version=plugin_version,
+    description=plugin_description,
+    author=plugin_author,
+    mail=plugin_author_email,
+    url=plugin_url,
+    license=plugin_license,
+    requires=plugin_requires,
+    additional_packages=plugin_additional_packages,
+    ignored_packages=plugin_ignored_packages,
+    additional_data=plugin_additional_data
 )
 
 if len(additional_setup_parameters):
-	from octoprint.util import dict_merge
-	setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
+    from octoprint.util import dict_merge
+    setup_parameters = dict_merge(
+        setup_parameters, additional_setup_parameters)
 
 setup(**setup_parameters)
