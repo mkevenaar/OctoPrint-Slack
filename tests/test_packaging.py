4f1252a0604fe6ff5f025f737c8c67359067001e
Maurice Kevenaar <maurice@kevenaar.name>
2026-09-01 21:16:14 +0200

    test: scope plugin entry point lookup

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -2,17 +2,14 @@ from importlib import metadata
 
 
 def test_octoprint_entry_point_is_installed():
-    entry_points = metadata.entry_points()
-    if hasattr(entry_points, "select"):
-        plugins = entry_points.select(group="octoprint.plugin", name="slack")
-    else:
-        plugins = [
-            entry_point
-            for entry_point in entry_points.get("octoprint.plugin", ())
-            if entry_point.name == "slack"
-        ]
+    distribution = metadata.distribution("OctoPrint-Slack")
+    plugins = [
+        entry_point
+        for entry_point in distribution.entry_points
+        if entry_point.group == "octoprint.plugin"
+        and entry_point.name == "slack"
+    ]
 
-    plugins = list(plugins)
     assert len(plugins) == 1
     assert plugins[0].value == "octoprint_slack"
 
